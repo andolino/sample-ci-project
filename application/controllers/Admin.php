7e40fa5db887603395f83e83ecf772270a80f604
Dondon <baisac.andolino@gmail.com>
2020-04-16 21:03:12 +0800

    ini commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-class Admin extends MY_Controller {
+class Admin extends MY_Controller 
 
 	function __construct(){
 		parent::__construct();
